718654b6793da8d40c687cacaa2460b0848ec44b
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-02-16 16:33:47 -0500

    :sparkles: feat: CTA section
    -Call-to-action component to hold the trial banner & form
    -Adjust app SCSS and styles.scss for updated imports
    -Fix up sign-up-component to include extra container for terms component
    -Terms component is separate cmp

--- intro-cmp-sign-up-form/src/app/app.module.ts
+++ intro-cmp-sign-up-form/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
 import { GridContainerComponent } from './grid-container/grid-container.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { CallToActionComponent } from './call-to-action/call-to-action.component';
+import { TrialBannerComponent } from './trial-banner/trial-banner.component';
+import { TermsComponent } from './terms/terms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeroComponent,
-    GridContainerComponent
+    GridContainerComponent,
+    SignUpFormComponent,
+    CallToActionComponent,
+    TrialBannerComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
---
+++ intro-cmp-sign-up-form/src/app/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sign-up-form',
+  templateUrl: './sign-up-form.component.html',
+  styleUrls: ['../../scss/components/_sign-up-form.component.scss']
+})
+export class SignUpFormComponent {
+
+}
