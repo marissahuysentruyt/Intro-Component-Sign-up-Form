e3bf6844568e5db83d972d3fb057bffc59b89f42
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-02-17 13:14:49 -0500

    :fire: refactor: sign-up cmp
    Fixed sign-up template HTML, adjusted some a11y contrast, BEM naming,
    SCSS, validation during input. Overall, more best practices should be
    in place

--- intro-cmp-sign-up-form/src/app/app.module.ts
+++ intro-cmp-sign-up-form/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeroComponent } from './hero/hero.component';
@@ -22,6 +23,7 @@ import { TermsComponent } from './terms/terms.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- intro-cmp-sign-up-form/src/app/sign-up-form/sign-up-form.component.ts
+++ intro-cmp-sign-up-form/src/app/sign-up-form/sign-up-form.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'sign-up-form',
   templateUrl: './sign-up-form.component.html',
   styleUrls: ['../../scss/components/_sign-up-form.component.scss']
 })
-export class SignUpFormComponent {
+export class SignUpFormComponent implements OnInit {
+  signUpForm!: FormGroup;
+  formSubmitted = false;
 
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.signUpForm = this.formBuilder.group({
+      firstName: this.formBuilder.control('',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\\/]+')
+        ])
+      ),
+      lastName: this.formBuilder.control('',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\\/]+')
+        ])
+      ),
+      email: this.formBuilder.control('', 
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+        ])
+      ), 
+      password: this.formBuilder.control('',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+        ])
+      ),
+    })
+  }
+
+  onSubmit(signUpForm: any) {
+    this.formSubmitted = true;
+    console.log('form submitted')
+    console.log(this.signUpForm.value)
+  }
 }
